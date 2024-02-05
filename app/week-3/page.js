5729cad3d7250e815a4ea918bc36f8a2bc76e8a3
lei.caps <leinajozsa.capili@edu.sait.ca>
2024-02-05 16:28:43 -0700

    Week 3 Assignment
    For Week 3's assignment, we made a static shopping list.

---
+++ app/week-3/page.js
@@ -0,0 +1,12 @@
+import ItemList from "./item-list";
+
+const Page = () => {
+    return (
+        <main className="bg-slate-950">
+            <h2 className="text-3xl font-bold m-2">Shopping List</h2>
+            <ItemList />
+        </main>
+    );
+};
+
+export default Page;
