10356078485480bf676dd2dc9d809be4f2d88713
lei.caps <leinajozsa.capili@edu.sait.ca>
2024-02-12 16:53:12 -0700

    Week 4 Assignment

--- app/page.js
+++ app/page.js
@@ -4,8 +4,12 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between">
-        <h1 class="text-4xl font-bold mb-5">CPRG 306: Web Development 2 - Assignments</h1>
-        <Link href="/week-2" class="hover-">Week 2 Assignment</Link>
+        <h1 className="text-4xl font-bold mb-5">CPRG 306: Web Development 2 - Assignments</h1>
+        <ul>
+          <li><Link href="/week-2">Week 2 Assignment</Link></li>
+          <li><Link href="/week-3">Week 3 Assignment</Link></li>
+          <li><Link href="/week-4">Week 4 Assignment</Link></li>
+        </ul>
       </div>
     </main>
   );
---
+++ app/week-4/page.js
@@ -0,0 +1,12 @@
+"use client";
+import NewItem from "./new-item";
+
+const Page = () => {
+    return (
+        <main className="flex justify-center w-full">
+            <NewItem />
+        </main>
+    );
+};
+
+export default Page;
