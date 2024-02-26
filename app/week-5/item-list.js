dec473189c27c6e9f37c42545dce6ad7f7b11f0d
lei.caps <leinajozsa.capili@edu.sait.ca>
2024-02-26 12:23:39 -0700

    Week 5 Assignment

---
+++ app/week-5/item-list.js
@@ -0,0 +1,40 @@
+"use client";
+
+import Item from "./item";
+import React, { useState } from 'react';
+import items from './items.json';
+
+const ItemList = () => {
+    const [sortBy, setSortBy] = useState('name');
+
+    const sortedItems = [...items].sort((a, b) => {
+        if (sortBy === 'name') {
+            return a.name.localeCompare(b.name);
+        } else if (sortBy === 'category') {
+            return a.category.localeCompare(b.category);
+        }
+        return 0;
+    });
+
+    return (
+        <div>
+            <div>
+                <label for="sort">Sort  by: </label>
+                <button class="bg-orange-700 p-1 m-2 w-28" onClick={() => setSortBy('name')}>Name</button>
+                <button class="bg-orange-700 p-1 m-2 w-28" onClick={() => setSortBy('category')}>Category</button>
+            </div>
+            <ul>
+                {sortedItems.map(item => (
+                    <Item
+                        key={item.id}
+                        name={item.name}
+                        quantity={item.quantity}
+                        category={item.category}
+                    />
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default ItemList;
---
+++ app/week-5/page.js
@@ -0,0 +1,16 @@
+"use client";
+
+import ItemList from "./item-list";
+
+const Page = () => {
+    return (
+        <main className="bg-slate-950">
+            <div class="m-4">
+                <h2 className="text-3xl font-bold m-2">Shopping List</h2>
+                <ItemList />
+            </div>
+        </main>
+    );
+};
+
+export default Page;
